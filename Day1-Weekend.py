91eb838658f1cef271971e9eabf496e909d5b251
Anisha-25 <53147663+Anisha-25@users.noreply.github.com>
2019-07-21 20:51:53 +0530

    Add files via upload

---
+++ Day1-Weekend.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World!")
+
+
+# In[2]:
+
+
+x=10
+y=5
+x+y
+
+
+# In[4]:
+
+
+name = "tony stark"
+print(name)
+
+
+# In[5]:
+
+
+name="tony stark"
+print(name.title())
+
+
+# In[6]:
+
+
+name = "TONY STARK"
+print(name.lower())
+
+
+# In[7]:
+
+
+name = "tony stark"
+print(name.upper())
+
+
+# In[9]:
+
+
+first_name = "Tony"
+last_name = "stark"
+
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+
+# In[10]:
+
+
+print(full_name.title())
+
+
+# In[11]:
+
+
+print("\tpython")
+
+
+# In[12]:
+
+
+print("Languages:\npython\nC\nJava")
+
+
+# In[ ]:
+
+
+
+
